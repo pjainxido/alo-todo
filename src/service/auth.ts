72b73d450f12fff8d2e200f53185fb03086de06f
pjainxido <pjainxido@gmail.com>
2022-07-28 22:09:46 +0900

    Add services

---
+++ src/service/auth.ts
@@ -0,0 +1,23 @@
+import { BASE_URL, ServiceResponse } from '../utils/request';
+
+export type LoginResponse = ServiceResponse & {
+  'access_token': string;
+};
+
+export const requestLogin = async (id: string, password: string): Promise<LoginResponse> => {
+  const options: RequestInit = {
+    method: 'POST',
+    headers: [['Content-Type', 'application/json']],
+    body: JSON.stringify({
+      user_id: id,
+      user_pw: password,
+    }),
+  };
+  const res = await fetch(BASE_URL + '/todo/login', options);
+  const data = await res.json();
+  if (res.ok && data.msg) {
+    return data;
+  } else {
+    throw Error(data);
+  }
+};
---
+++ src/service/todoList.ts
@@ -0,0 +1,32 @@
+import { Todo } from '../context/TodoListProvider/todoList.type';
+import { getCookie } from '../utils/cookie';
+import { BASE_URL, ServiceResponse } from '../utils/request';
+
+export type TodoListResponse = {
+  'current-user': string;
+  'todo-list': Todo[];
+};
+
+export type MeResponse = ServiceResponse & {
+  result: TodoListResponse;
+};
+
+export const requestMe = async (): Promise<MeResponse> => {
+  const option: RequestInit = {
+    headers: {
+      Authorization: `Bearer ${getCookie('access_token')}`,
+    },
+  };
+
+  const res = await fetch(BASE_URL + '/todo/me', option);
+  const data = await res.json();
+  if (res.ok) {
+    console.log(data);
+    return data;
+  } else {
+    throw Error(data);
+  }
+  // .then(({ msg, result }: MeResponse) => {
+  //   return msg === 'success' ? result : [];
+  // });
+};
