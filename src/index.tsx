29885567abc16567a589fe6f88bfd690a3d8d5b4
pjainxido <pjainxido@gmail.com>
2022-07-28 18:46:17 +0900

    Add ContextProvider  in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 import TodoListProvider from './context/TodoListProvider';
+import UserProvider from './context/UserProvider';
 import Layout from './components/Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <TodoListProvider>
-      <Layout>
-        <App />
-      </Layout>
-    </TodoListProvider>
-  </BrowserRouter>
+  <>
+    <UserProvider>
+      <TodoListProvider>
+        <BrowserRouter>
+          <Layout>
+            <App />
+          </Layout>
+        </BrowserRouter>
+      </TodoListProvider>
+    </UserProvider>
+  </>
 );
